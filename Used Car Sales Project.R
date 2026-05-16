693be7a8820b0578f305c4e3d465b5eb2964297a
Ankurs2021 <90215977+Ankurs2021@users.noreply.github.com>
2026-05-17 00:48:16 +0530

    Add files via upload

--- Used Car Sales Project.R
+++ Used Car Sales Project.R
@@ -53,8 +53,8 @@ df_clean %>%
                                        if_else(brand %in% Premium_niche, "Premium Niche", "Common")))) -> df_clean
 
 
-table(df_clean$gearbox) # There is a category without any name. It is not NA
-table(df_clean$notRepairedDamage) # There is a category without any name. It is not NA
+table(df_clean$gearbox) # There is a category without any name. It is not NA, now converted to Unknown
+table(df_clean$notRepairedDamage) # There is a category without any name. It is not NA, now converted to Unknown
 
 summary(df_clean$price) # There is a price at 0 value and 99999999 value in USD, likely to be outliers
 summary(df_clean$odometer) # Appears normal
@@ -90,6 +90,21 @@ df_trend %>%
        y = "Average price ($)",
        color = "Brand") +
   theme_minimal()
+
+
+df_trim %>% 
+  filter(odometer %in% c(5000, 50000)) %>% 
+  group_by(brand) %>% 
+  summarise(val1 = mean(price[odometer == 5000], na.rm = T),
+            val2 = mean(price[odometer == 50000], na.rm = T),
+            diff = ((val2-val1)/val1*100)) %>% 
+  ggplot(aes(x = brand, y = diff, fill = diff)) + geom_bar(stat = 'identity') +
+  coord_flip() +
+  labs(x = "Mileage",
+       y = "Cost in USD",
+       fill = "") + 
+  theme_minimal() +
+  theme(legend.position = "")
 
 # Price Retention by Brand (Retention rate)
 retention <- df_trim %>% 
@@ -168,3 +183,84 @@ retention3 <- df_trim %>%
         axis.text.y = element_text(size = 10))
 
 retention3
+
+# Comparing against the Mean Retention Rate
+df_trim %>% 
+  filter(odometer %in% c(5000, 50000)) %>% 
+  mutate(overall = mean(price)) %>% 
+  mutate(ret = (overall / mean(price[odometer == 5000], na.rm = T))*100) %>% 
+  group_by(pr_category) %>% 
+  #mutate(overall = mean(price)) %>% 
+  summarise(start_price = mean(price[odometer == 5000], na.rm = T),
+            mid_price = mean(price[odometer == 50000], na.rm = T),
+            ret_rate = (mid_price / start_price)*100,
+            avg = ret) %>% 
+  arrange(-ret_rate) %>% 
+  head(10) %>% 
+  ggplot(aes(x = reorder(pr_category, ret_rate), y = ret_rate, fill = pr_category)) + geom_bar(stat = 'identity') +
+  coord_flip() +
+  geom_hline(aes(yintercept = avg), linetype = 'dashed', color = "gray", size = 1) +
+  geom_text(aes(label = paste0(round(ret_rate,1),"%")), position = position_dodge(width = 0.5), hjust = -0.15) +
+  labs(title = "Cost retention of cars after 50K kms",
+       subtitle = "Compared to 5K kms",
+       x = "Price category",
+       y = "Retention rate (%)",
+       caption = "Gray line indicates the overall retention rate (171%) from 5K to 50K driven cars") +
+  theme_minimal() +
+  theme(legend.position = "",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        plot.title = element_text(face = 'bold', size = 18),
+        axis.text.y = element_text(size = 10))
+
+  
+df_trim %>%
+  filter(brand %in% c("Volkswagen", "Opel", "Bmw", "Audi")) %>%
+  filter(yearOfRegistration >= 2000) %>%
+  mutate(overall = mean(price)) %>% 
+  mutate(ret = (overall / mean(price[odometer == 5000], na.rm = T))*100) %>% 
+  group_by(br_category) %>% 
+  #mutate(overall = mean(price)) %>% 
+  summarise(start_price = mean(price[odometer == 5000], na.rm = T),
+            mid_price = mean(price[odometer == 50000], na.rm = T),
+            ret_rate = (mid_price / start_price)*100,
+            avg = ret) %>% 
+  arrange(-ret_rate) %>% 
+  head(10) %>% 
+  ggplot(aes(x = reorder(br_category, ret_rate), y = ret_rate, fill = br_category)) + geom_bar(stat = 'identity') +
+  coord_flip() +
+  geom_hline(aes(yintercept = avg), linetype = 'dashed', color = "gray", size = 1) +
+  geom_text(aes(label = paste0(round(ret_rate,1),"%")), position = position_dodge(width = 0.5), hjust = -0.15) +
+  labs(title = "Cost retention of cars after 50K kms",
+       subtitle = "Compared to 5K kms",
+       x = "Price category",
+       y = "Retention rate (%)",
+       caption = "Gray line indicates the overall retention rate (171%) from 5K to 50K driven cars") +
+  theme_minimal() +
+  theme(legend.position = "",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.x = element_blank(),
+        plot.title = element_text(face = 'bold', size = 18),
+        axis.text.y = element_text(size = 10))
+
+
+# Price retention by selected car brands across years (2000 & later)
+df_trim %>%
+  filter(brand %in% c("Volkswagen", "Opel", "Bmw", "Audi")) %>%
+  filter(yearOfRegistration >= 2000) %>%
+  ggplot(aes(x = factor(yearOfRegistration), y = price, fill = brand)) +
+  geom_boxplot(outlier.shape = NA) +
+  labs(title = "Price Evolution by Registration Year", x = "Year", y = "Price") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 20000)) # Focus on the bulk of the data
+
+# Price retention by car brand categories across years (2000 & later)
+df_trim %>%
+  filter(yearOfRegistration >= 2000) %>%
+  ggplot(aes(x = factor(yearOfRegistration), y = price, fill = br_category)) +
+  geom_boxplot(outlier.shape = NA) +
+  labs(title = "Price Evolution by Registration Year", subtitle = "by Brand category", x = "Year", y = "Price", fill = "Brand Category") +
+  theme_minimal() +
+  coord_cartesian(ylim = c(0, 20000)) # Focus on the bulk of the data
